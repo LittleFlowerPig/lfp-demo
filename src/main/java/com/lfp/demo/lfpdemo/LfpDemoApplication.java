a94574748c43671964a95ab37f7fd6de33e799fe
zhutao <zhutao@qudian.com>
2019-01-24 19:32:09 +0800

    版本初建

---
+++ src/main/java/com/lfp/demo/lfpdemo/LfpDemoApplication.java
@@ -0,0 +1,14 @@
+package com.lfp.demo.lfpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LfpDemoApplication.class, args);
+    }
+
+}
+
